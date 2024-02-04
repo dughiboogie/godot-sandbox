1c758361071b627ea07f324d158d5285a7f8f341
Marco Dugatto <dughi.boogie@gmail.com>
2024-02-04 23:25:33 +0100

    [ghouls-n-dungeons] Basic melee attack implementation

--- 01_Prototypes/ghouls-n-dungeons/Scripts/Player.gd
+++ 01_Prototypes/ghouls-n-dungeons/Scripts/Player.gd
@@ -22,6 +22,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var camera = $Head/Camera3D
 @onready var wall_detection = $Head/WallDetectionRayCast3D
 @onready var step_detection = $Head/StepDetectionRayCast3D
+@onready var animation_player = $AnimationPlayer
 
 
 func _ready():
@@ -33,29 +34,36 @@ func _unhandled_input(event):
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
-	
+
+
+func _unhandled_key_input(_event):
 	# DEBUGGING PURPOSES
-	elif Input.is_action_just_pressed("restart"):
+	if Input.is_action_just_pressed("restart"):
 		get_tree().reload_current_scene()
-	elif Input.is_action_just_pressed("quit"):
+	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("attack"):
+		animation_player.play("attack")
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Sprint
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -83,7 +91,7 @@ func _physics_process(delta):
 	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
 	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
 	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-
+	
 	move_and_slide()
 
 
@@ -92,3 +100,8 @@ func _headbob(time) -> Vector3:
 	pos.y = sin(time * BOB_FREQUENCY) * BOB_AMPLITUDE
 	pos.x = cos(time * BOB_FREQUENCY / 2) * BOB_AMPLITUDE
 	return pos
+
+
+func _on_weapon_hitbox_body_entered(body):
+	if body.is_in_group("enemy"):
+		print("Enemy hit!")
